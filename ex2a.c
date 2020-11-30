4dbe09196fff9e818e862298f7317d711f6e4e7a
BarIfrah <71547715+BarIfrah@users.noreply.github.com>
2020-11-30 17:42:27 +0200

    Update ex2a.c

--- ex2a.c
+++ ex2a.c
@@ -1,10 +1,9 @@
-
 //ex2a
 // this program creates a child that generates 10 random prime numbers.
 // there is no user input. only play and watch the miracle.
 // the child gets 3 seconds to find 10 different prime numbers.
 // child is dead if he's unable to provide 3 different prime numbers in
-// 3 seconds. in this case the output will be "kill kid"
+// 3 seconds. in this case the output will be "bad boy"
 // if the child comes up with 10 numbers, it prints them.
 
 // we may get the wanted output- 10 numbers & good boy
@@ -26,23 +25,20 @@ pid_t proccessID;
 //------------------------------------------------------------------------
 void do_dad(pid_t );
 void do_son(void);
-void terminate (void);
-
+void terminate (void);  
 void catch_alarm(int);
-void * catch_sigterm(int arr[]);    // * to pass the array
+void * catch_sigterm(int []);    // * to pass the array
 void child_handler(int );
-int is_prime(int num);
-int same_number_exist(const int arr[],int size,int num);
-void print_numbers(int arr[]);
+int is_prime(int possible_prime);
+int same_number_exist(const int [],int ,int );
+void print_numbers(int []);
 //------------------------------------------------------------------------
 int main () {
     signal(SIGALRM, catch_alarm);
     signal(SIGCHLD, child_handler);
-    srand(17);
-
-    proccessID = fork();
-
-    if (proccessID < 0){
+    srand(17);              // as requested.
+    proccessID = fork();    // creates child.
+    if (proccessID < 0){    //failure
         terminate();
     }
     if (proccessID > 0){
@@ -60,7 +56,7 @@ void terminate() {
 }
 //------------------------------------------------------------------------
 void do_dad (pid_t son_pid) {
-    alarm(3);
+    alarm(3);           // gives son 3 seconds before termination
     pause();
     alarm(0);
 
@@ -70,17 +66,17 @@ void do_dad (pid_t son_pid) {
 }
 //------------------------------------------------------------------------
 void do_son () {
-    struct sigaction act;
-    int arr[10];
-    int num,i;
+    struct sigaction act;       // a struct of sigaction 
+    int array_of_primes[10];
+    int lottory_prime,i;
     for(i = 0; i < 10 ; i++) {
-        num = rand();
-        while(!is_prime(num) && same_number_exist(arr, i, num) ){
-            num = rand();
+        lottory_prime = rand();
+        while(!is_prime(lottory_prime) && same_number_exist(array_of_primes, i, lottory_prime) ){
+            lottory_prime = rand();
         }
-        arr[i]=num;
+        array_of_primes[i]=lottory_prime;
     }
-    act.sa_handler = catch_sigterm(arr);
+    act.sa_handler = catch_sigterm(array_of_primes);
     sigfillset(&act.sa_mask);
     act.sa_flags = 0;
     sigaction(SIGTERM,&act,NULL);
@@ -88,38 +84,38 @@ void do_son () {
 }
 //------------------------------------------------------------------------
 void catch_alarm(int signum) {
-    puts("Kill Kid\n");
+    puts("bad boy\n");
     kill(proccessID,SIGTERM);
 }
 //------------------------------------------------------------------------
-int is_prime(int num) {
+int is_prime(int possible_prime) {
     int i;
-    for(i = 2 ; i <= sqrt(num) ; i++) {
-        if(num % i == 0)
+    for(i = 2 ; i <= sqrt(possible_prime) ; i++) {
+        if(possible_prime % i == 0)
             return FALSE;
     }
     return TRUE;
 }
 //------------------------------------------------------------------------
-int same_number_exist(const int arr[],int size,int num) {
+int same_number_exist(const int array_of_primes[],int size,int num) {
     int i;
     for(i = 0 ; i < size ; i++){
-        if(arr[i]==num)
+        if(array_of_primes[i] == num)
             return FALSE;
     }
     return TRUE;
 }
 //------------------------------------------------------------------------
-void print_numbers(int arr[]) {
+void print_numbers(int array_of_primes[]) {
     int i;
     for(i = 0 ; i < 10 ; i++) {
-        printf("%d ",arr[i]);
+        printf("%d\n",array_of_primes[i]);
     }
 }
 //------------------------------------------------------------------------
-void * catch_sigterm(int arr[]) {
-    void * a;
-    print_numbers(arr);
+void * catch_sigterm(int array_of_primes[]) {
+    void * a;               // a only to silence a warning
+    print_numbers(array_of_primes);
     return a;
 }
 //------------------------------------------------------------------------
